0d66eda30a655d832431860376d93ca209403557
Mohamed Ibrahim Basha <tajibrahim2021@gmail.com>
2021-12-14 23:32:46 +0530

    SwaggerUI visible for all environment

--- EmployeeAPI/EmployeeAPI/Startup.cs
+++ EmployeeAPI/EmployeeAPI/Startup.cs
@@ -48,19 +48,20 @@ namespace EmployeeAPI
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-                                  
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "EmployeeAPI v1");
+                c.RoutePrefix = string.Empty;
+
+            });
 
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
-                app.UseSwagger();
-                app.UseSwaggerUI(c =>
-                {
-
-                    c.SwaggerEndpoint("/swagger/v1/swagger.json", "EmployeeAPI v1");
-                    c.RoutePrefix = string.Empty;
-
-                });
+                
+                
             }
 
             app.UseHttpsRedirection();
